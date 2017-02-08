dca5ca130c5060d0d14c26d0776507244c03e602
Wataru Noguchi <wnoguchi@pg1x.io>
2017-02-09 08:53:08 +0900

    Change Title.

--- src/conf.py
+++ src/conf.py
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'PG1X Documents'
+project = u'PG1X'
 copyright = u'2017, Wataru NOGUCHI'
 author = u'Wataru NOGUCHI'
 
